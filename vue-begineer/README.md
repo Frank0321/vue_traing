83061685101d40b1d203ece49f0016cc9773cb13
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-11-28 17:03:42 +0800

    新增 lesson2 的內容

--- vue-begineer/README.md
+++ vue-begineer/README.md
@@ -1,19 +1,42 @@
 # vue-begineer
 
-## Project setup
-```
-yarn install
-```
+## lesson2 常用指令
 
-### Compiles and hot-reloads for development
+### main.js 介紹
+1. 引用 vue
+2. 引用 app.vue 用它的內容來替換 div id = app
+3. 構建 vue 實例
+```javascript
+new Vue({
+    el: '#app',
+    render: function (c){
+        returnc(App);
+    }
+})
 ```
-yarn serve
-```
+- el : 渲染內容的目的地
 
-### Compiles and minifies for production
-```
-yarn build
-```
+### v-text
+- 是元素的 innerText 只能在雙標籤中使用
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### v-html
+- 是元素的 innerHTML 不能包含{{xxx}}
+
+### v-if
+- 元素是否移除或插入 
+- false 則會被當成註解，移除掉
+
+### v-show
+- 元素是否顯示或隱藏
+- false 則元素的屬性 會被加上 display: none
+
+### v-model
+- 雙向改變
+
+### v-bind
+- 單向改變
+- v-bind 的值會受外部影響，但改變他的值，不會改變到外部值
+
+### v-model
+
+ 
---
+++ vue-begineer/src/views/Lesson2.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <pre>
+      <h2>v-test</h2>
+            <span v-text="text"></span>
+            <input type="text" name="" v-text="text">
+      <h2>v-html</h2>
+            <span v-html="html"></span>
+      <h2>v-if</h2>
+            <div v-if="isShow" style="height: 100px; background-color: red"></div>
+      <h2>v-show</h2>
+            <div v-show="isShow" style="height: 100px; background-color: green"></div>
+      <h2>v-model</h2>
+            <input type="text" name="" v-model="mText">
+            {{mText}}
+            <input type="text" name="" v-bind:value="mText">
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Lesson2",
+  data() {
+    return {
+      text: '我是 v-text 玩的東西',
+      html: `
+              <ul>
+                <li>哈哈</li>
+                <li>呵呵</li>
+              </ul>
+            `,
+      isShow: false,
+      mText: '我是 v-model',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
