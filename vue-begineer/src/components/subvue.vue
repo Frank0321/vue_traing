2ecc292a8b6d36472daf834005e97be767525394
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-12-08 01:03:05 +0800

    新增 lesson15 ref 的使用 (未完成)

---
+++ vue-begineer/src/components/subvue.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    我是 sub vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: "subvue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue-begineer/src/views/lesson15.vue
+++ vue-begineer/src/views/lesson15.vue
@@ -4,16 +4,21 @@
     <div ref="myDiv">
       {{text}}
     </div>
+    <subvue ref="sub"> </subvue>
   </div>
 </template>
 
 <script>
+import subvue from "../components/subvue";
 export default {
   name: "lesson15",
   data(){
     return{
       text: 123,
     }
+  },
+  components: {
+    subvue
   },
   //駔見創建後，數據已經完成初始化，但是 DOM 還未生成
   created() {
@@ -22,7 +27,12 @@ export default {
   //數據裝載 DOM 上後，各種數據已經就位，將數據渲染到 DOM 上，DOM已經生成
   mounted() {
     console.log('mounted :', this.$refs.myDiv);
-    this.$refs.myDiv.innerHTML ='哈哈';
+    //涉及 DOM 的操作
+    // this.$refs.myDiv.innerHTML ='哈哈';
+    //涉及 數據 的操作
+    this.text = '呵呵';
+
+    console.log('sub :', this.$refs.sub.$el);
   }
 }
 </script>
